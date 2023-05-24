7b67edc3a58f1f62ba80d42e9116991c38b8360c
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-24 18:42:34 -0300

    switch

---
+++ switchh.java
@@ -0,0 +1,17 @@
+public class switchh {
+    public static void main(String[] args) {
+       int opcao=2;
+
+       switch (opcao) {
+        case 1:
+            System.out.println("Voce selecionou a opção 1");
+            break;
+        case 2: 
+            System.out.println("Voce selecionou a opção 2");
+            break;
+        case 3:
+            System.out.println("Voce selecionou a opção 3");
+            break;
+       }
+    }
+}
