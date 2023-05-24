f8088b62fc09a47ff98b6646551fa507eec4e0ae
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-23 22:45:49 -0300

    condicional simples(operador ternario)

---
+++ condicionalSimples.java
@@ -0,0 +1,7 @@
+public class condicionalSimples {
+    public static void main(String[] args) {
+       boolean fimDeSemana= true;
+       String mensagem= fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+       System.out.println(mensagem);
+    }
+}
