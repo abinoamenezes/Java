5a9fd2756d557ee9fea694dcc5ea2f7fc985a7b0
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-23 22:24:26 -0300

    configuração de ambiente, helloWorld, variaveis e operadores aritmeticos

---
+++ variaveis.java
@@ -0,0 +1,17 @@
+public class variaveis {
+    public static void main(String[] args) {
+        String nome= "Abinoa";
+        int a=2;
+        int b=3;
+        int soma= a+b;
+        int subtracao =a-b;
+        int multi= a*b;
+        float divisao= (float) a/b;
+        System.out.println("Bem vindo " + nome);
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multi);
+        System.out.println(divisao);
+
+    }
+}
