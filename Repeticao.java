8201e603a3ae63db0d6b83bb7ca3e7f2f9231baf
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-25 10:55:41 -0300

    estrutura de repetição

---
+++ Repeticao.java
@@ -0,0 +1,18 @@
+public class Repeticao {
+    public static void main(String[] args) {
+       for (int i=0; i<5; i++) {
+        System.out.println(i);
+       }
+
+       int n=0;
+       while (n<5) {
+        System.out.println("hello world");
+        n++;
+       }
+       int j=0;
+       do {
+        System.out.println("heelo");
+        j++;
+       } while (j<0);
+    }
+}
