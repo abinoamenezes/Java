e72a72361063b7179b7fd7a5042b2842c4750dff
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-25 10:56:21 -0300

    datas

--- datas.java
+++ datas.java
@@ -1,6 +1,6 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.time.format.TextStyle;
 import java.util.Locale;
 
@@ -9,7 +9,7 @@ public class datas {
        String nome= "Abinoa";
        LocalDate dataHoje= LocalDate.now();
       DayOfWeek diaSemana= dataHoje.getDayOfWeek();
-       LocalDateTime hora= LocalDateTime.now();
+   
        String diaSemanaPT= diaSemana.getDisplayName(TextStyle.FULL, Locale.getDefault());
 
         System.out.printf("olá %s. hoje é %s", nome, diaSemanaPT);
