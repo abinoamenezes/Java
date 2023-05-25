7ad4632ed01bec164039a572d29bc0194a14ff5f
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-25 13:28:24 -0300

    funcoes

---
+++ funcoes.java
@@ -0,0 +1,15 @@
+public class funcoes {
+    public static void main(String[] args) {
+        
+        System.out.println(somar(2,3));
+        
+    }
+
+    public static  int somar(int a, int b) {
+        int resultado= a+b;
+        return resultado;
+    }
+}
+
+
+
