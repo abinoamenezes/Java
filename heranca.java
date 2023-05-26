d0295d88b4178219f6375f6437f062fe9400937b
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-26 15:08:51 -0300

    heranca

---
+++ heranca.java
@@ -0,0 +1,10 @@
+import seres.Cachorro;
+
+public class heranca {
+    public static void main(String[] args) {
+        Cachorro cachorro= new Cachorro("thorzinho");
+
+        cachorro.emitirSom();
+        cachorro.latir();
+    }
+}
