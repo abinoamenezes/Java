ecc2117d769abf59cdd1bfbaa68dce537aa3b91f
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-26 15:12:03 -0300

    heranca

---
+++ seres/Animal.java
@@ -0,0 +1,16 @@
+package seres;
+
+public class Animal {
+    private String nome;
+
+    public Animal(String nome) {
+        this.nome=nome;
+    }
+
+    public void emitirSom() {
+        System.out.println("o animal está emitindo um som");
+    }
+    
+
+}
+
