814d55d53564de3d7cc0cc344d7396cc9ef51106
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-26 15:10:40 -0300

    classes

---
+++ classes.java
@@ -0,0 +1,11 @@
+import veiculos.Carros;
+
+public class classes {
+    public static void main(String[] args) {
+        Carros car= new Carros("ferrari", "296 GTB", 2010);
+       car.acelerar();
+       car.frear();
+    }
+}
+
+
