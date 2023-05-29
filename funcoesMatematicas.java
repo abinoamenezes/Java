424c34cb0a467b64b3e8aaefa5f4e1a6cf3f80d6
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-29 18:01:59 -0300

    funcoesMatematicas

---
+++ funcoesMatematicas.java
@@ -0,0 +1,11 @@
+public class funcoesMatematicas {
+    public static void main(String[] args) {
+        double potencia= Math.pow(2, 10);
+        System.out.println(potencia);
+        double raiz= Math.sqrt(729);
+        System.out.println(raiz);
+        int max=Math.max(10, 50);
+        System.out.println(max);
+
+    }
+}
