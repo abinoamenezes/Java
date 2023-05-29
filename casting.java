ab6980816fc82a1128acb154a21c2e9852245c44
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-29 17:38:48 -0300

    casting

---
+++ casting.java
@@ -0,0 +1,21 @@
+public class casting {
+    public static void main(String[] args) {
+        //cassting implicito
+        int x= 10;
+
+        System.out.println(x);
+
+        double y= x;
+
+        System.out.println(y);
+
+        //casting explicito
+
+        double g=10.5;
+        System.out.println(g);
+        int f= (int) g;
+        System.out.println(f);
+
+
+    }
+}
