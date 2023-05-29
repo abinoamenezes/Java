fec7a847d60d4ea439a7978f97e2d8b00dc9e2c6
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-05-29 17:31:41 -0300

    saida de dados

---
+++ saida_De_Dados.java
@@ -0,0 +1,13 @@
+public class saida_De_Dados {
+    public static void main(String[] args) {
+        System.out.print("hello ");
+        System.out.print("world");
+        System.out.println("");
+
+        String name= "Abino";
+        int age= 20;
+
+        System.out.printf("nome: %s, age: %d", name, age);
+
+    }
+}
